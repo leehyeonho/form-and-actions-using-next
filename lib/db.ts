569344cec68ca782cbf6cce19c694b2c0676dd7a
leehyeonho <dlgusgh0812@gmail.com>
2025-05-11 17:39:59 +0900

    fix: 오류 수정

--- lib/db.ts
+++ lib/db.ts
@@ -2,13 +2,4 @@ import { PrismaClient } from '@prisma/client'
 
 const db = new PrismaClient();
 
-async function test() {
-    const user = await db.user.create({
-        data: {
-            username: "bichobi",
-            password: "1111"
-        }
-    })
-}
-
 export default db;
